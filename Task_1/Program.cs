1375adc23bcd036d423b7c12de4d1ea5be9920f9
Unseed <unseedevil@gmail.com>
2023-01-04 11:29:55 +0300

    Решение первой задачи

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,50 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
+
+// Создаем массив который заполняем рандомными трех значными цифрами
+
+int[] array = GetRandomArray(5, 100, 999);
+
+PrintArray(array);
+FindHonestNumber(array);
+
+int ReadInt (string argument)
+{
+    System.Console.Write($"Input {argument}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int[] GetRandomArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int [length];
+    Random random = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    for (int i= 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+}
+
+int FindHonestNumber (int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            sum += 1;
+        }
+    }System.Console.Write($"Количество четных чисел в массиве = {sum}.");
+    return sum;
+}
+
